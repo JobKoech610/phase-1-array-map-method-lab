2c41591e412d25a35fafc442a3ca51862f526c96
Job koech <jobkoech610@gmail.com>
2023-03-21 23:16:30 +0300

    complete assignment

--- index.js
+++ index.js
@@ -12,16 +12,18 @@ const tutorials = [
 ];
 
 function titleCased() {
+  //.map() allow us to copy the original array and we can manipulate it
   return tutorials.map(function(tutorial) {
-    //split a sentence into seperate words using .split() method
+    //split a sentence into seperate words using .split() method e.g '['what, does, the, this, keyword, mean?']
     let words = tutorial.split(" ");
     console.log(words);
 // loop through the words and return the first character into uppercase using .upperCase() method. 
-// slice(1) helps to remove the first char in a word and we concatinate to the remaining part to the upperCase letter.
+// slice(1) helps to remove the first char in a word and we concatenate to the remaining part to the upperCase letter.
     for (let i = 0; i < words.length; i++) {
       words[i] = words[i][0].toUpperCase() + words[i].slice(1);
     }
-    //join() helps us to join th split sentence
+    console.log(words);
+    //join() helps us to join the split sentence into one complete sentence
     return words.join(" ");
   });
 }
