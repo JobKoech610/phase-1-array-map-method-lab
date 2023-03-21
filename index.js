a7b14abe06a3906f781965f8870497ce9560d050
Job koech <jobkoech610@gmail.com>
2023-03-21 18:09:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function(tutorial) {
+    //split a sentence into seperate words using .split() method
+    let words = tutorial.split(" ");
+    console.log(words);
+// loop through the words and return the first character into uppercase using .upperCase() method. 
+// slice(1) helps to remove the first char in a word and we concatinate to the remaining part to the upperCase letter.
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    //join() helps us to join th split sentence
+    return words.join(" ");
+  });
 }
+
+console.log(titleCased());
+
+
+
+
+
+
+// function titleCase(str) {
+//   return str.toLowerCase().split(' ').map(function(word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+// titleCase("I'm a little tea pot");
